a5e593f420ea9090d434ed7db685ed005af39690
ephymucira <146751770+ephymucira@users.noreply.github.com>
2024-03-05 21:03:47 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,237 @@
+import flet as ft
+from flet import *
+from datetime import datetime
+import sqlite3
+
+class FormContainer(UserControl):
+   def __init__(self,func):
+      self.func = func
+      super().__init__()
+
+   def build(self):
+      
+      return Container(
+         width=200,
+         height=80,
+         bgcolor="bluegrey500",
+         opacity=0,
+         border_radius=40,
+         margin= margin.only(left=20, right=20),
+         animate=animation.Animation(400,"decelerate"),
+         animate_opacity=200,
+         padding=padding.only(top=45, bottom=45),
+         content=Column(
+            horizontal_alignment=CrossAxisAlignment.CENTER,
+            controls=[
+               TextField(
+                  height=48,
+                  width=255,
+                  filled=True,
+                  color="black",
+                  text_size= 12,
+                  border_color="transparent",
+                  hint_text="Description...",
+                  hint_style= TextStyle(size=11, color="black" ),
+
+               ),
+               IconButton(
+                  content=Text("Add Task"),
+                  width = 100,
+                  height=44,
+                  on_click=self.func,
+                  style=ButtonStyle(
+                     bgcolor={"":"black"},
+                     shape={
+                        "": RoundedRectangleBorder(radius=8),
+                     }
+                  )
+               )
+            ]
+
+         )
+
+      )
+# my_object =FormContainer()
+# my_object.build()
+
+#class to generate a task
+class CreateTask(UserControl):
+   def __init__(self, task:str, date:str):
+      self.task = task
+      self.date = date
+      super().__init__()
+
+      def TaskDeleteEdit(self, name, color):
+         return IconButton(
+            icon=name,
+            width=30,
+            icon_size=18,
+            icon_color=color,
+            opacity=0,
+            animate_opacity=200,
+            on_click=None # make changes
+
+         )
+
+
+      def build(self):
+         return Container(
+            width=200,
+            height=60,
+            border=border.all(0.85, "white54"),
+            border_radius=8,
+            on_hover=None,
+            clip_behavior=ClipBehavior.HARD_EDGE,
+            padding=10,
+            content=Row(
+               alignment=MainAxisAlignment.SPACE_BETWEEN,
+               controls=[
+                  Column(
+                     spacing =1,
+                     alignment=MainAxisAlignment.CENTER,
+                     controls=[
+                        Text(value=self.task, size=10),
+                        Text(value=self.date, size=9, color='white54'),
+
+                     ],
+                  ),
+                  Row(
+                     spacing=0,
+                     alignment=MainAxisAlignment.CENTER,
+                     controls=[
+                        self.TaskDeleteEdit(icons.DELETE_ROUNDED, "red500"),
+                        self.TaskDeleteEdit(icons.EDIT_ROUNDED, "white70"),
+                     ]
+                  )
+
+               ],
+            ),
+         )
+
+
+def main(page:Page):
+ page.horizontal_alignment = 'center'
+ page.vertical_alignment = 'center'
+
+ def AddTaskToScreen(e):
+    #everytime the user adds a task, we need to fetch the data and output it to the main column
+
+    dateTime = datetime.now().strftime("%b %d, %Y %I:%M")
+
+    #CHECKING IF THERE IS ANY CONTENT IN THE TEXTFIELD
+    if form.content.controls[0].value:
+       _main_column_.controls.append(
+          #here we create an instance of CreateTask() class.
+          CreateTask(
+             form.content.controls[0].value,
+             dateTime,
+             
+          )
+          
+
+          
+       )
+       _main_column_.update()
+       
+       
+
+    
+     
+
+ #function to show and hide form container
+ def CreateToDoTask(e):
+    if form.height != 200:
+       form.height , form.opacity = 200 ,1
+       form.update()
+    else:
+       form.height , form.opacity = 80,0
+       form.update()
+       
+     
+
+
+ _main_column_ = Column(
+    scroll='hidden',
+    expand=True,
+    alignment=MainAxisAlignment.START,
+    controls=[
+       Row(
+          alignment=MainAxisAlignment.SPACE_BETWEEN,
+          
+          controls=[
+             Text(
+                "TO-DO Items",
+                size=18,
+                weight="bold"
+             ),
+             IconButton(
+                icons.ADD_CIRCLE_ROUNDED,
+                icon_size=18,
+                on_click=lambda e: CreateToDoTask(e),
+             ),
+             
+          ],
+       ),
+       Divider(height=0,color="white24"),
+    ]
+    
+ )
+
+
+
+ page.add(
+    
+   
+   Container(
+     width=1500,
+     height=800,
+     margin=10,
+     bgcolor="bluegrey900",
+     padding=padding.only(bottom=30),
+     alignment=alignment.center,
+     content=
+       Row(
+         alignment=MainAxisAlignment.CENTER,
+         vertical_alignment=CrossAxisAlignment.CENTER,
+         controls=[
+           Container(
+             margin = margin.only(bottom = 30),
+             width=280,
+             height=550,
+             bgcolor="#0f0f0f",
+             border_radius = 40,
+             border = border.all(0.5, "white"),
+             padding = padding.only(top=15, left=20, right=20),
+             clip_behavior = ClipBehavior.HARD_EDGE,
+
+             content=Column(
+                
+                alignment=MainAxisAlignment.CENTER,
+                expand=True,
+                controls=[
+                   _main_column_,
+                   FormContainer(lambda e:AddTaskToScreen(e))
+                   
+                ]
+             )
+
+           )
+         ],
+       ),
+     
+   ),
+   
+   
+    
+ )
+ page.update()
+
+
+#there is an error here
+ form = page.controls[0].content.controls[0].content.controls[1].controls[0]
+
+
+
+if __name__ =="__main__":
+
+    ft.app(target = main)
